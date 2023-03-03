a3033870bc22e63b405d2e6f722cbcd46957e9ce
Enamhossain <bhaianam9@gmail.com>
2023-03-03 22:15:22 +0600

    work

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000
